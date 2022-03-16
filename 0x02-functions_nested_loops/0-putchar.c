eaa65f90cc91adca3ed9b137fc25daa56e3fafd4
root <root@Mamila.localdomain>
2022-03-16 12:11:42 +0300

    putcar updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,11 @@
 
 int main(void)
 {
-char c[10] = "_putchar";
+char c[] = "_putchar";
 int i = 0;
-while (i < 9)
+for (i = 0; i < 8; i++)
 {
 _putchar(c[i]);
-i++;
 }
 _putchar('\n');
 return (0);
